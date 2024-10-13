189084ba0f95f213d249d4d37fa8bc488357019b
Matin Vilan <matinvilan1994@gmail.com>
2024-10-13 04:47:35 +0330

    feat: from now protecting all routes with next auth handled

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,41 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Please enter your username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Please enter your password",
+        },
+      },
+      async authorize(credentials) {
+        // hard code to retrive data
+        // because we don't have database
+        const user = { id: "1", name: "Matin", password: "123456" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
