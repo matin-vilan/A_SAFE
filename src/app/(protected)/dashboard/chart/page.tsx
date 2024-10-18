30d178a2175842ee4c5e5b14efd41db6a975fc1e
Matin Vilan <matinvilan1994@gmail.com>
2024-10-18 04:52:37 +0330

    chore: some changes in chart component and typography

--- src/app/(protected)/dashboard/chart/page.tsx
+++ src/app/(protected)/dashboard/chart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import ClientOnly from "@components/clientOnly";
 import Button from "@design/Button";
+import Typography from "@design/Typography";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -14,6 +15,9 @@ const ChartPage = () => {
   return (
     <ClientOnly>
       <Button onClick={() => router.push("/dashboard")}>Go back</Button>
+      <Typography className="text-red-600">
+        THIS IS A <b>CLIENT SIDE</b> PAGE
+      </Typography>
       <ChartContainer />
     </ClientOnly>
   );
