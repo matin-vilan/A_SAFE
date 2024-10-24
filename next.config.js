e3676694fad94fc13323475dfc17524e9149389a
Matin Vilan <matinvilan1994@gmail.com>
2024-10-24 22:35:13 +0330

    MAIN: add env config to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_USER_API_URL: process.env.NEXT_PUBLIC_USER_API_URL,
+    NEXT_PUBLIC_GOLD_API_URL: process.env.NEXT_PUBLIC_GOLD_API_URL,
+  },
 };
 
 module.exports = nextConfig;
