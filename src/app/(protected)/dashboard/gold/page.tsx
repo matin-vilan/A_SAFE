5774f2d553b7c590a3193df3dcdb00939b4eee46
Matin Vilan <matinvilan1994@gmail.com>
2024-10-24 21:58:50 +0330

    chore: add some more description

--- src/app/(protected)/dashboard/gold/page.tsx
+++ src/app/(protected)/dashboard/gold/page.tsx
@@ -2,10 +2,20 @@
 import React from "react";
 import GoldContainer from "./components/GoldContainer";
 import ClientOnly from "@components/clientOnly";
+import Typography from "@design/Typography";
+import Flex from "@design/Flex";
 
 const GoldChartPage = () => {
   return (
     <ClientOnly>
+      <Flex vertical>
+        <Typography variant="normal">
+          Date filter cannot include more than four days
+        </Typography>
+        <Typography variant="description">
+          For more date filter you need to purchase API
+        </Typography>
+      </Flex>
       <GoldContainer />
     </ClientOnly>
   );
